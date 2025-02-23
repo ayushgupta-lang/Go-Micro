78ed73c602536b56bea16d6984efb016e2be4012
Ayush Gupta <ayushibansal9927@gmail.com>
2025-02-23 13:45:07 +0530

    Add the mongodb and logger service to docker compose yaml and make file

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -49,21 +49,32 @@ func main() {
 	}
 
 	// start web server
-	go app.serve()
+	// go app.serve()
 
-}
-
-func (app *Config) serve() {
+	log.Println("Starting service on port", webPort)
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic()
 	}
+
 }
+
+// func (app *Config) serve() {
+// 	srv := &http.Server{
+// 		Addr:    fmt.Sprintf(":%s", webPort),
+// 		Handler: app.routes(),
+// 	}
+
+// 	err := srv.ListenAndServe()
+// 	if err != nil {
+// 		log.Panic()
+// 	}
+// }
 
 func connectToMongo() (*mongo.Client, error) {
 	// create  connection options
@@ -79,5 +90,8 @@ func connectToMongo() (*mongo.Client, error) {
 		log.Println("Error connecting", err)
 		return nil, err
 	}
+
+	log.Println("Connected to mongo!")
+
 	return c, nil
 }
